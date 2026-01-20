31c8ca9eb6e9416a880f9aebf711a041e3ad5458
Ashikur07 <ashik.ict.iu@gmail.com>
2026-01-20 13:08:43 +0600

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-
+import Experience from "../components/Experience";
+import Overlay from "../components/Overlay";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full">
-      <h1 className="text-9xl">3D Car Project Setup Ready</h1>
+    <main className="relative w-full">
+      
+      {/* 3D Scene (Fixed Background) */}
+      <div className="fixed top-0 left-0 w-full h-screen z-0">
+        <Experience />
+      </div>
+
+      {/* Scrollable Content (Foreground) */}
+      <Overlay />
+      
     </main>
   );
 }
