08651dcd6e7931bc7b02678ad445bc2287b5a937
Ashikur07 <ashik.ict.iu@gmail.com>
2026-01-23 01:48:19 +0600

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar"; // ইমপোর্ট করো
+import Performance from "@/components/Performance";
 
 export default function Home() {
   return (
     <main className="relative">
       <Navbar /> {/* সবার উপরে ন্যাপবার */}
       <Hero />
+      <Performance />
       
       {/* টেম্পোরারি নেক্সট সেকশন প্লেসহোল্ডার */}
       <div className="h-screen bg-black flex items-center justify-center text-white/20">
